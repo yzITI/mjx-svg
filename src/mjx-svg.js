e366adbf3cac33d15180df7f3ad88351beb14d4e
Phantomlsh <phantomlsh@outlook.com>
2021-11-12 19:20:37 -0800

    Decouple (#1)
    * On the way of decoupling
    
    * complete decouple the algorithm

--- src/mjx-svg.js
+++ src/mjx-svg.js
@@ -1,3 +1,78 @@
+let serialNum = 0
+const serial = () => String(serialNum++)
+
+function parsePaths (defs) {
+  const res = {}
+  for (const path of defs.children) res[path.id.replace(/MJX-\d*?-/, '')] = path.attributes.d.value
+  return res
+}
+
+function parseAST (node, res) {
+  const attr = a => node.attributes[a]?.value
+  const n = { name: node.nodeName, transform: [0, 0, 1] }
+  if (n.name == 'rect') {
+    n.type = 'rect'
+    n.attributes = { width: attr('width'), height: attr('height'), x: attr('x'), y: attr('y') }
+  }
+  if (n.name == 'use') {
+    n.type = attr('xlink:href').replace(/#MJX-\d*?-/, '')
+    n.attributes = { pathId: n.type }
+  }
+  if (n.name == 'g') {
+    n.mml = attr('data-mml-node')
+  }
+  const transform = attr('transform')
+  if (transform) {
+    const translate = transform.match(/translate\((.*?),(.*?)\)/)
+    const scale = transform.match(/scale\((.*?)\)/)
+    if (translate) {
+      n.transform[0] = Number(translate[1])
+      n.transform[1] += Number(translate[2])
+    }
+    if (scale) n.transform[2] = Number(scale[1])
+  }
+  n.children = []
+  for (const c of node.children) n.children.push(parseAST(c, res))
+  const id = serial()
+  res[id] = n
+  return id
+}
+
+function parseLeaves (id, AST, leaves, transform = [0, 0, 1]) {
+  const node = AST[id]
+  const newTransform = [transform[0] + node.transform[0]*transform[2], transform[1] + node.transform[1]*transform[2], transform[2]*node.transform[2]]
+  if (!node.children.length) {
+    leaves[id] = { name: node.name, type: node.type, mml: node.mml, attributes: node.attributes, transform: newTransform }
+  }
+  for (const c of node.children) parseLeaves(c, AST, leaves, newTransform)
+}
+
+function createElement (leaf, options, paths) {
+  if (leaf.name == 'use') {
+    leaf.element = document.createElementNS("http://www.w3.org/2000/svg", 'path')
+  }
+  if (leaf.name == 'rect') {
+    leaf.element = document.createElementNS("http://www.w3.org/2000/svg", 'rect')
+  }
+  leaf.element.style.transition = options.transition
+  leaf.element.style.opacity = 0
+  setTimeout(() => { leaf.element.style.opacity = 1 }, options.createDelay)
+  updateElement(leaf, paths)
+}
+
+function updateElement (leaf, paths) {
+  if (leaf.name == 'use') {
+    leaf.element.style.d = `path("${paths[leaf.attributes.pathId]}")`
+  }
+  if (leaf.name == 'rect') {
+    leaf.element.style.width = leaf.attributes.width + 'px'
+    leaf.element.style.height = leaf.attributes.height + 'px'
+    leaf.element.style.x = leaf.attributes.x + 'px'
+    leaf.element.style.y = leaf.attributes.y + 'px'
+  }
+  leaf.element.style.transform = `translate(${leaf.transform[0]}px, ${leaf.transform[1]}px) scale(${leaf.transform[2]})`
+}
+
 export default class {
   constructor (el, options = {}) {
     this.options = {
@@ -13,8 +88,8 @@ export default class {
     const w = this.svg.attributes.width.value, h = this.svg.attributes.height.value
     this.svg.removeAttribute('width')
     this.svg.removeAttribute('height')
-    this.svg.style.width = w
-    this.svg.style.height = h
+    this.svg.style.width = `calc(${this.options.scale} * ${w})`
+    this.svg.style.height = `calc(${this.options.scale} * ${h})`
     this.svg.style.overflow = 'visible'
     // remove rubbish
     this.svg.removeChild(this.svg.firstChild)
@@ -23,116 +98,60 @@ export default class {
   }
   async update (tex) {
     const svg = this.getSVG(tex), root = svg.children[1]
-    this.svg.style.width = svg.attributes.width.value
-    this.svg.style.height = svg.attributes.height.value
+    const w = svg.attributes.width.value, h = svg.attributes.height.value
+    this.svg.style.width = `calc(${this.options.scale} * ${w})`
+    this.svg.style.height = `calc(${this.options.scale} * ${h})`
     this.svg.style.verticalAlign = svg.style.verticalAlign
     this.svg.setAttribute('viewBox', svg.attributes.viewBox.value)
 
-    this.parsePaths(svg.children[0])
-    const AST = this.parseAST(root.children[0]) // new AST
-
-    const leaves = [] // new leaves
-    this.parseLeaves(AST, leaves)
+    this.paths = parsePaths(svg.children[0])
+    const AST = {}, leaves = {} // new AST and leaves
+    const rootId = parseAST(root.children[0], AST)
+    parseLeaves(rootId, AST, leaves)
+    this.diff(AST, leaves)
     this.render(leaves)
-
     this.AST = AST
   }
   getSVG (tex) {
     return MathJax.tex2svg(tex, { scale: 3 }).children[0]
   }
-  parsePaths (defs) {
-    this.paths = {}
-    for (const path of defs.children) this.paths[path.id.replace(/MJX-\d*?-/, '')] = path.attributes.d.value
-  }
-  parseAST (node) {
-    const attr = a => node.attributes[a]?.value
-    const res = { name: node.nodeName, transform: [0, 0, 1] }
-    if (res.name == 'rect') {
-      res.type = 'rect'
-      res.attributes = { width: attr('width'), height: attr('height'), x: attr('x'), y: attr('y') }
+  diff (AST, leaves) {
+    const types = {}
+    for (const id in this.leaves) {
+      const l = this.leaves[id]
+      if (!types[l.type]) types[l.type] = []
+      types[l.type].push(id)
     }
-    if (res.name == 'use') {
-      res.type = attr('xlink:href').replace(/#MJX-\d*?-/, '')
-      res.attributes = { pathId: res.type }
+    for (const id in leaves) {
+      const l = leaves[id]
+      if (types[l.type] && types[l.type].length) l.map = types[l.type].shift()
     }
-    if (res.name == 'g') {
-      res.type = attr('data-mml-node')
-    }
-    const transform = attr('transform')
-    if (transform) {
-      const translate = transform.match(/translate\((.*?),(.*?)\)/)
-      const scale = transform.match(/scale\((.*?)\)/)
-      if (translate) {
-        res.transform[0] = Number(translate[1])
-        res.transform[1] += Number(translate[2])
-      }
-      if (scale) res.transform[2] = Number(scale[1])
-    }
-    res.children = []
-    for (const c of node.children) res.children.push(this.parseAST(c))
-    return res
-  }
-  parseLeaves (node, leaves, transform = [0, 0, 1]) {
-    const newTransform = [transform[0] + node.transform[0]*transform[2], transform[1] + node.transform[1]*transform[2], transform[2]*node.transform[2]]
-    if (!node.children.length) {
-      leaves.push({ name: node.name, type: node.attributes.pathId || node.name, attributes: node.attributes, transform: newTransform })
-      return
-    }
-    for (const c of node.children) this.parseLeaves(c, leaves, newTransform)
-  }
-  updateElement (leaf) {
-    if (leaf.name == 'use') {
-      leaf.element.style.d = `path("${this.paths[leaf.attributes.pathId]}")`
-    }
-    if (leaf.name == 'rect') {
-      leaf.element.style.width = leaf.attributes.width + 'px'
-      leaf.element.style.height = leaf.attributes.height + 'px'
-      leaf.element.style.x = leaf.attributes.x + 'px'
-      leaf.element.style.y = leaf.attributes.y + 'px'
-    }
-    leaf.element.style.transform = `translate(${leaf.transform[0]}px, ${leaf.transform[1]}px) scale(${leaf.transform[2]})`
-  }
-  createElement (leaf) {
-    if (leaf.name == 'use') {
-      leaf.element = document.createElementNS("http://www.w3.org/2000/svg", 'path')
-    }
-    if (leaf.name == 'rect') {
-      leaf.element = document.createElementNS("http://www.w3.org/2000/svg", 'rect')
-    }
-    leaf.element.style.transition = this.options.transition
-    leaf.element.style.opacity = 0
-    setTimeout(() => { leaf.element.style.opacity = 1 }, this.options.createDelay)
-    this.updateElement(leaf)
   }
   render (leaves) {
-    const types = {}
-    // get types of old leaves
-    for (const l of this.leaves) {
-      if (!types[l.type]) types[l.type] = []
-      types[l.type].push(l)
-      l.deprecating = true
-    }
-    // loop over new leaves
-    for (const l of leaves) {
-      if (types[l.type] && types[l.type].length) { // reuse
-        const leaf = types[l.type].shift()
-        leaf.deprecating = false
-        leaf.attributes = l.attributes
+    const generation = serial()
+    for (const id in leaves) {
+      const l = leaves[id]
+      if (l.map) { // reuse
+        const leaf = this.leaves[l.map]
         leaf.transform = l.transform
-        this.updateElement(leaf)
-      } else { // create new leaf
-        this.createElement(l)
-        this.leaves.push(l)
+        leaf.attributes = l.attributes
+        leaf.generation = generation
+        updateElement(leaf, this.paths)
+      } else { // create new
+        createElement(l, this.options, this.paths)
+        l.generation = generation
+        this.leaves[id] = l
         this.svg.firstChild.appendChild(l.element)
       }
     }
-    // remove deprecated
-    for (const l of this.leaves) {
-      if (l.deprecating) {
+    // remove old
+    for (const id in this.leaves) {
+      const l = this.leaves[id]
+      if (l.generation != generation) {
         l.element.style.opacity = 0
         setTimeout(() => { this.svg.firstChild.removeChild(l.element) }, this.options.removeDelay)
+        delete this.leaves[id]
       }
     }
-    this.leaves = this.leaves.filter(x => !x.deprecating)
   }
 }
